71e8cf45427429bd08035190e5e7a619dcf0cb43
DylanPerkins <dp011400@gmail.com>
2023-05-18 16:24:39 -0400

    Added application command that works!
    Edited main command to be a slash command

--- translation.py
+++ translation.py
@@ -1,5 +1,7 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
+import asyncio
 import deepl
 from dotenv import load_dotenv
 import os
@@ -14,10 +16,26 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="/", intents=intents)
 
-@commands.hybrid_command()
-async def translate(ctx: commands.Context, message_id: int):
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
     try:
-        message = await ctx.channel.fetch_message(message_id)
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands")
+    except Exception as e:
+        print(f"An error occurred while syncing: {str(e)}")
+
+
+# TODO: Gets error `AttributeError: 'Interaction' object has no attribute 'send' ` 
+@bot.tree.command()
+async def translate(ctx: commands.Context, message_id: str):
+    try:
+        # Fetch the message from the channel
+        
+        if not message_id.isdigit():
+            return await ctx.send("Please enter a valid message ID.")
+
+        message = await ctx.fetch_message(int(message_id))
         content = message.content
 
         translator = deepl.Translator(auth_key)
@@ -31,5 +49,15 @@ async def translate(ctx: commands.Context, message_id: int):
     except Exception as e:
         await ctx.send(f"An error occurred while translating the message: {str(e)}")
 
-bot.add_command(translate)
+@bot.tree.context_menu(name="Translate to English")
+async def translate_to_english(interaction: discord.Interaction, message: discord.Message):
+    content = message.content
+
+    translator = deepl.Translator(auth_key)
+
+    result = translator.translate_text(content, target_lang="EN-US")
+    translated_text = result.text
+
+    await interaction.response.send_message(f"Translated message: {translated_text}", ephemeral=True)
+
 bot.run(f"{bot_token}")

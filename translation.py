715febf287e7faac12ed1d61ec71ed592cdf5198
DylanPerkins <dp011400@gmail.com>
2023-05-19 11:13:17 -0400

    Fixed! It all works now!

--- translation.py
+++ translation.py
@@ -16,6 +16,7 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="/", intents=intents)
 
+
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
@@ -26,16 +27,15 @@ async def on_ready():
         print(f"An error occurred while syncing: {str(e)}")
 
 
-# TODO: Gets error `AttributeError: 'Interaction' object has no attribute 'send' ` 
 @bot.tree.command()
 async def translate(ctx: commands.Context, message_id: str):
     try:
         # Fetch the message from the channel
-        
+
         if not message_id.isdigit():
             return await ctx.send("Please enter a valid message ID.")
 
-        message = await ctx.fetch_message(int(message_id))
+        message = await ctx.channel.fetch_message(int(message_id))
         content = message.content
 
         translator = deepl.Translator(auth_key)
@@ -43,11 +43,12 @@ async def translate(ctx: commands.Context, message_id: str):
         result = translator.translate_text(content, target_lang="FR")
         translated_text = result.text
 
-        await ctx.send(f"Translated message: {translated_text}")
+        await ctx.response.send_message(f"Translated message: {translated_text}")
     except discord.errors.NotFound:
-        await ctx.send(f"Sorry, I couldn't find a message with the ID of  {message_id} in this channel.")
+        await ctx.response.send_message(f"Sorry, I couldn't find a message with the ID of  {message_id} in this channel.")
     except Exception as e:
-        await ctx.send(f"An error occurred while translating the message: {str(e)}")
+        await ctx.response.send_message(f"An error occurred while translating the message: {str(e)}")
+
 
 @bot.tree.context_menu(name="Translate to English")
 async def translate_to_english(interaction: discord.Interaction, message: discord.Message):

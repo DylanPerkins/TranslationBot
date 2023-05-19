5565569a7300755774785fbdad10096d8189205e
DylanPerkins <dp011400@gmail.com>
2023-05-19 11:38:22 -0400

    Added 4 more context menus for different languages
    Fixed the issue with /translate !

--- translation.py
+++ translation.py
@@ -30,26 +30,30 @@ async def on_ready():
 @bot.tree.command()
 async def translate(ctx: commands.Context, message_id: str):
     try:
-        # Fetch the message from the channel
-
+        # Check if the supplied message ID is a number
         if not message_id.isdigit():
             return await ctx.send("Please enter a valid message ID.")
 
+        # Fetch the message from the channel
         message = await ctx.channel.fetch_message(int(message_id))
         content = message.content
 
+        # Translate the message using the DeepL API
         translator = deepl.Translator(auth_key)
-
         result = translator.translate_text(content, target_lang="FR")
         translated_text = result.text
 
+        # Send the translated message
         await ctx.response.send_message(f"Translated message: {translated_text}")
     except discord.errors.NotFound:
         await ctx.response.send_message(f"Sorry, I couldn't find a message with the ID of  {message_id} in this channel.")
     except Exception as e:
         await ctx.response.send_message(f"An error occurred while translating the message: {str(e)}")
 
+# Context menus (right click menus)
+# Note: Max of 5
 
+# Translate to English
 @bot.tree.context_menu(name="Translate to English")
 async def translate_to_english(interaction: discord.Interaction, message: discord.Message):
     content = message.content
@@ -59,6 +63,59 @@ async def translate_to_english(interaction: discord.Interaction, message: discor
     result = translator.translate_text(content, target_lang="EN-US")
     translated_text = result.text
 
-    await interaction.response.send_message(f"Translated message: {translated_text}", ephemeral=True)
+    await interaction.response.send_message(f"Translated message to __English__:\n{translated_text}", ephemeral=True)
+
+
+# Translate to Spanish
+@bot.tree.context_menu(name="Translate to Spanish")
+async def translate_to_english(interaction: discord.Interaction, message: discord.Message):
+    content = message.content
+
+    translator = deepl.Translator(auth_key)
+
+    result = translator.translate_text(content, target_lang="ES")
+    translated_text = result.text
+
+    await interaction.response.send_message(f"Translated message to __Spanish__:\n{translated_text}", ephemeral=True)
+
+
+# Translate to Chinese (Simplified)
+@bot.tree.context_menu(name="Translate to Chinese")
+async def translate_to_english(interaction: discord.Interaction, message: discord.Message):
+    content = message.content
+
+    translator = deepl.Translator(auth_key)
+
+    result = translator.translate_text(content, target_lang="ZH")
+    translated_text = result.text
+
+    await interaction.response.send_message(f"Translated message to __Chinese (Simplified)__:\n{translated_text}", ephemeral=True)
+
+
+# Translate to French
+@bot.tree.context_menu(name="Translate to French")
+async def translate_to_english(interaction: discord.Interaction, message: discord.Message):
+    content = message.content
+
+    translator = deepl.Translator(auth_key)
+
+    result = translator.translate_text(content, target_lang="FR")
+    translated_text = result.text
+
+    await interaction.response.send_message(f"Translated message to __French__:\n{translated_text}", ephemeral=True)
+
+
+# Translate to Ukrainian
+@bot.tree.context_menu(name="Translate to Ukrainian")
+async def translate_to_english(interaction: discord.Interaction, message: discord.Message):
+    content = message.content
+
+    translator = deepl.Translator(auth_key)
+
+    result = translator.translate_text(content, target_lang="UK")
+    translated_text = result.text
+
+    await interaction.response.send_message(f"Translated message to __Ukrainian__:\n{translated_text}", ephemeral=True)
+
 
 bot.run(f"{bot_token}")

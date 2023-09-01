b94e05e8a30bb507309a72fa1fdc010f9c62c63c
DylanPerkins <dp011400@gmail.com>
2023-09-01 16:44:44 -0400

    WORKS! TY ROMEY

--- cogs/history_translation.py
+++ cogs/history_translation.py
@@ -29,15 +29,15 @@ class TranslateMessageHistory(commands.Cog):
             message_id_top = int(top_message_link.split("/")[-1])
             message_id_bottom = int(bottom_message_link.split("/")[-1])
 
-            # Check if the supplied message ID is a number
-            if not isinstance(message_id_top, int) or message_id_top <= 0:
-                return await ctx.send(
-                    "Please enter a valid starting message link.", ephemeral=True
-                )
-            if not isinstance(message_id_bottom, int) or message_id_bottom <= 0:
-                return await ctx.send(
-                    "Please enter a valid ending message link.", ephemeral=True
-                )
+            try:
+                top_message = await ctx.channel.fetch_message(message_id_top)
+            except discord.NotFound:
+                return await ctx.send("Message not found. Please try again.")
+
+            try:
+                bottom_message = await ctx.channel.fetch_message(message_id_bottom)
+            except discord.NotFound:
+                return await ctx.send("Message not found. Please try again.")
 
             # Get the message objects from the message IDs
             message_objects = []
@@ -51,31 +51,12 @@ class TranslateMessageHistory(commands.Cog):
             ):
                 message_objects.append(message)
 
-            # Find the message data between the message links
-            if not message_objects or message_objects[0].id != message_id_top:
-                # Fetch the 'top' message link data
-                try:
-                    top_message = await ctx.channel.fetch_message(message_id_top)
-                    message_objects.insert(0, top_message)
-                except discord.NotFound:
-                    pass
-
-            if not message_objects or message_objects[-1].id != message_id_bottom:
-                # Fetch the 'bottom' message link data
-                try:
-                    bottom_message = await ctx.channel.fetch_message(message_id_bottom)
-                    message_objects.append(bottom_message)
-                except discord.NotFound:
-                    pass
+            message_objects.insert(0, top_message)
+            message_objects.append(bottom_message)
 
             message_content = "\n".join(message.content for message in message_objects)
 
-            if not message_content:
-                return await ctx.send(
-                    "No message content found between the specified message links. Maybe try different links?",
-                    ephemeral=True,
-                )
-
+            # DeepL API Call
             if language is None:
                 language = "EN-US"
             else:
@@ -93,18 +74,15 @@ class TranslateMessageHistory(commands.Cog):
                 f"**Top Message Link:** {top_message_link}\n"
                 f"**Bottom Message Link:** {bottom_message_link}"
             )
-            await ctx.send(links_message)
+            links_message_object = await ctx.send(links_message)
 
-            # Fetch the message id of the links_message
-            links_message_id = fetch_message_id(self, links_message)
-
-            # Create a public thread on the original message
-            thread = await create_thread(ctx, int(links_message_id))
+            thread = await links_message_object.create_thread(
+                name=f"Translation Thread by {ctx.author}",
+                reason="Thread for message translation",
+            )
 
             # Send the original and translated content in the new public thread
-            await thread.send(
-                f"### Original Message Content:\n{message_content}"
-            )
+            await thread.send(f"### Original Message Content:\n{message_content}")
             await thread.send(
                 f"### Translated Message Content to __{language_name}__:\n{translated_message}"
             )
@@ -119,19 +97,6 @@ class TranslateMessageHistory(commands.Cog):
                 f"Whoops! An error occurred while translating the message:\n{str(e)}",
                 ephemeral=True,
             )
-
-async def create_thread(ctx: commands.Context, message_id: int):
-    thread_name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    thread = await discord.message.Message.create_thread(
-        self=ctx.message.channel.fetch_message(message_id),
-        name=f"Translation Thread by {ctx.author} - {thread_name}",
-        reason="Thread for message translation",
-    )
-    return thread
-
-# Fetch the message id of the links_message
-async def fetch_message_id(self, message):
-    return message.id
 
 
 async def setup(bot):
